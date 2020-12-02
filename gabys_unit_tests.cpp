58841f889cfa0905de2aaeee3300718831171445
galva041 <galva041@ucr.edu>
2020-12-02 01:21:06 -0800

    tests complete

---
+++ gabys_unit_tests.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "mult_test.hpp"
+#include "div_test.hpp"
+#include "pow_test.hpp"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ mult_test.hpp
@@ -0,0 +1,54 @@
+#include "gtest/gtest.h"
+#include "lab04/base.hpp"
+#include "lab04/Mult.hpp"
+#include "iterator.hpp"
+
+TEST(MultBinaryIteratorTest, getLeft) {
+    Base* three = new Op(3);
+    Base* five = new Op(5);
+    Mult* fifteen = new Mult(three, five);
+    EXPECT_EQ(fifteen->get_left()->stringify(), "3.000000");
+}
+
+TEST(MultBinaryIteratorTest, getRight) {
+    Base* three = new Op(3);
+    Base* five = new Op(5);
+    Mult* fifteen = new Mult(three, five);
+    EXPECT_EQ(fifteen->get_right()->stringify(), "5.000000");
+}
+
+TEST(MultBinaryIteratorTest, TwoOperands) {
+    Base* three = new Op(3);
+    Base* five = new Op(5);
+    Mult* fifteen = new Mult(three, five);
+
+    Iterator* myI = fifteen->create_iterator();
+    bool check1 = false, check2 = false;
+    for(myI->first(); !myI->is_done(); myI->next()) {
+        if (myI->current() == fifteen->get_left()) {
+            check1 = true;
+        } else if (myI->current() == fifteen->get_right()) {
+            check2 = true;
+        }
+    }
+    EXPECT_EQ(check1, true);
+    EXPECT_EQ(check2, true);
+}
+
+TEST(MultBinaryIteratorTest, TwoNegOperands) {
+    Base* three = new Op(-3);
+    Base* five = new Op(-5);
+    Mult* fifteen = new Mult(three, five);
+
+    Iterator* myI = fifteen->create_iterator();
+    bool check1 = false, check2 = false;
+    for(myI->first(); !myI->is_done(); myI->next()) {
+        if (myI->current() == fifteen->get_left()) {
+            check1 = true;
+        } else if (myI->current() == fifteen->get_right()) {
+            check2 = true;
+        }
+    }
+    EXPECT_EQ(check1, true);
+    EXPECT_EQ(check2, true);
+}
